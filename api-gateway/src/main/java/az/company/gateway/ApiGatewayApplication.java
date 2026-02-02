1a08f4fd51b012cf35253291e3fb6c2c6d0e8755
Paşa Şərifov <pasa.serifov@etsn.local>
2026-02-02 18:30:23 +0400

    change api gateway yaml to bean

--- api-gateway/src/main/java/az/company/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/az/company/gateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package az.company.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,22 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("ms-product", r -> r
+                        .path("/products/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://ms-product"))
+                .route("ms-order", r -> r
+                        .path("/orders/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8081"))
+                .route("ms-payment", r -> r
+                        .path("/payments/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
 
 }
